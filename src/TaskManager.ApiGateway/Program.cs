05dccfdb2ea756206ea20b1d3fe2b74a4cc25667
kaxhc <kaxhcar@gmail.com>
2024-08-18 15:19:24 -0300

    api gateway

---
+++ src/TaskManager.ApiGateway/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace OcelotApiGw
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var builder = WebHost.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(s => s.AddSingleton(builder))
+                    .UseStartup<Startup>()
+                    .ConfigureAppConfiguration((hostingContext, config) =>
+                    {
+                        config
+                            .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                            .AddJsonFile("ocelot.json")
+                            .AddEnvironmentVariables();
+                    });
+
+            var host = builder.Build();
+            return host;
+        }
+    }
+}
